4e1603fff26acf5d1e1c05ef5f06c6d58cb7e591
pjy0509 <80870882+pjy0509@users.noreply.github.com>
2021-05-27 14:35:29 +0900

    add json function

--- Tenant.java
+++ Tenant.java
@@ -4,14 +4,23 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Scanner;
 import java.util.Map.Entry;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 public class Tenant {
 	String name = null;
 	String add = null;
 	String tel = null;
 	String famInfo = null;
-	String[] tenantInfo = new String[3];
 	
+	String[] tenantInfo = new String[3];
+	JSONObject jsonObject = new JSONObject();
+    JSONArray personArray = new JSONArray();
+    JSONObject personInfo = new JSONObject();
+    String jsonInfo = "";
+    
 	Scanner scan = new Scanner(System.in);
 	
 	
@@ -28,15 +37,26 @@ public class Tenant {
 			tel = null;
 			famInfo = null;
 			
+			personInfo = new JSONObject();
 			
 			System.out.println("이름을 입력하세요.");
 			name = scan.next();
+			personInfo.put("name", name);
+			
 			System.out.println("주소를 입력하세요.");
 			add = scan.next();
+			personInfo.put("add", add);
+			
 			System.out.println("전화번호를 입력하세요.");
 			tel = scan.next();
+			personInfo.put("tel", tel);
+			
 			System.out.println("가족정보를 입력하세요.");
 			famInfo = scan.next();
+			personInfo.put("famInfo", famInfo);
+			
+			personArray.add(personInfo);
+		    jsonObject.put("persons", personArray);
 			
 			add = "주소:"+add;
 			tenantInfo[0] = "이름:"+name;
